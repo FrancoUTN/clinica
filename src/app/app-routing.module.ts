aec65d2f8bc740a6556d30851dd89adb63971c6b
unknown <cataniafrancoar@gmail.com>
2022-06-07 18:11:00 -0300

    Especialistas en salud

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { LoginComponent } from './bienvenida/login/login.component';
+import { RegistroComponent } from './bienvenida/registro/registro.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'bienvenida', pathMatch: 'full' },
+  { path: 'bienvenida', component: BienvenidaComponent },
+  { path: 'login', component: RegistroComponent },
+  { path: 'registro', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+import { LoginComponent } from './bienvenida/login/login.component';
+import { RegistroComponent } from './bienvenida/registro/registro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    LoginComponent,
+    RegistroComponent
   ],
   imports: [
     BrowserModule,
