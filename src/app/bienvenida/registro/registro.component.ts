ec2e22a0b402ddbfb34dbbde7f2511d21ba29a1a
unknown <cataniafrancoar@gmail.com>
2022-06-08 00:22:52 -0300

    Registro tabs

--- src/app/bienvenida/registro/registro.component.ts
+++ src/app/bienvenida/registro/registro.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-registro',
@@ -6,10 +8,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registro.component.scss']
 })
 export class RegistroComponent implements OnInit {
+  esPaciente:boolean = false;
+  error:string = '';
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authService:AuthService) {
+  }
 
   ngOnInit(): void {
   }
+  
+  signUp(value:any) {
+    this.authService.SignUp(value.email, value.password)
+      .then(() => this.router.navigateByUrl('page/home')) // Definir
+      .catch(razon => this.error = razon.message);
+  }
 
+  clickPacienteHandler() {
+    this.esPaciente = true;
+  }
+
+  clickEspecialistaHandler() {
+    this.esPaciente = false;
+  }
 }
