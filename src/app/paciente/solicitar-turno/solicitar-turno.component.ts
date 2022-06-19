e5a8b60a1f02f6549470258d07e98dc0da62c885
unknown <cataniafrancoar@gmail.com>
2022-06-19 06:45:23 -0300

    Antes de tocar Firestore

--- src/app/paciente/solicitar-turno/solicitar-turno.component.ts
+++ src/app/paciente/solicitar-turno/solicitar-turno.component.ts
@@ -10,13 +10,34 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 export class SolicitarTurnoComponent implements OnInit {
   especialidades: string[] = ["Nutrición", "Dermatología", "Traumatología"];
   especialistas: any[] = [];
-  paso1: boolean = true;
+  paso1: boolean = false;
   paso2: boolean = false;
-  paso3: boolean = false;
+  paso3: boolean = true;
+
+  horarios: Date[] = [];
+
+  mediaHora: number = 1800;
+  unaHora: number = 3600;
+  unDia: number = 86400;
+  quinceDias: number = 1296000;
+
 
   constructor(private usuarioService: UsuarioService) { }
 
   ngOnInit(): void {
+  }
+
+  rellenarHorarios() {
+    var someDate = new Date();
+    var numberOfDaysToAdd = 15;
+    var result = someDate.setDate(someDate.getDate() + numberOfDaysToAdd);
+    console.log(new Date(result))
+  }
+
+  addDays(date: number, days: number) {
+    var result = new Date(date);
+    result.setDate(result.getDate() + days);
+    return result;
   }
 
   onEspecialidadSeleccionadaHandler() {
