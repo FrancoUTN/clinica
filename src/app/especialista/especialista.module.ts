d92dc02c6ac091cc7d1bfb6c6ad0b6622e9bcf5a
unknown <cataniafrancoar@gmail.com>
2022-06-19 07:21:13 -0300

    Base de perfil y horarios

--- src/app/especialista/especialista.module.ts
+++ src/app/especialista/especialista.module.ts
@@ -4,16 +4,24 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { EspecialistaComponent } from './especialista.component';
 import { MenuEspecialistaComponent } from './menu-especialista/menu-especialista.component';
+import { MiPerfilComponent } from './mi-perfil/mi-perfil.component';
+import { MisHorariosComponent } from './mis-horarios/mis-horarios.component';
 
 
 const routes: Routes = [
-  { path: '', component: EspecialistaComponent },
+  { path: '', component: EspecialistaComponent, children: [
+    { path: 'mi-perfil', component: MiPerfilComponent, children: [
+      { path: 'mis-horarios', component: MisHorariosComponent }
+    ]}
+  ]}
 ];
 
 @NgModule({
   declarations: [
     EspecialistaComponent,
-    MenuEspecialistaComponent
+    MenuEspecialistaComponent,
+    MiPerfilComponent,
+    MisHorariosComponent
   ],
   imports: [
     CommonModule,
---
+++ src/app/especialista/mi-perfil/mi-perfil.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-mi-perfil',
+  templateUrl: './mi-perfil.component.html',
+  styleUrls: ['./mi-perfil.component.scss']
+})
+export class MiPerfilComponent implements OnInit {
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+}
