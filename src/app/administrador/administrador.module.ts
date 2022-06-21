281071c421e111c6d5e26a0a9d5c37e9048f1191
unknown <cataniafrancoar@gmail.com>
2022-06-21 13:33:59 -0300

    Mi perfil administrador

--- src/app/administrador/administrador.module.ts
+++ src/app/administrador/administrador.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 
@@ -7,11 +6,13 @@ import { UsuariosComponent } from './usuarios/usuarios.component';
 import { AdministradorComponent } from './administrador.component';
 import { MenuAdministradorComponent } from './menu-administrador/menu-administrador.component';
 import { RegistroAdministradorComponent } from './registro-administrador/registro-administrador.component';
+import { MiPerfilComponent } from '../shared/mi-perfil/mi-perfil.component';
 
 
 const routes: Routes = [
   { path: '', component: AdministradorComponent, children: [
     { path: 'usuarios', component: UsuariosComponent },
+    { path: 'mi-perfil', component: MiPerfilComponent }
     ]
   }
 ];
