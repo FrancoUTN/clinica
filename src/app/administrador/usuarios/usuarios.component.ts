4489b1781201c7335fe85f783547737c7a38ef0e
unknown <cataniafrancoar@gmail.com>
2022-06-23 06:10:18 -0300

    Template Pacientes

--- src/app/administrador/usuarios/usuarios.component.ts
+++ src/app/administrador/usuarios/usuarios.component.ts
@@ -79,16 +79,8 @@ export class UsuariosComponent implements OnInit {
     );
   }
 
-  verMiHistoriaClinicaHandler(paciente: any) {
+  verHistoriaClinicaHandler(paciente: any) {
     this.pacienteSeleccionado = paciente;
-    // this.verHistoriaClinica = false;
-
-    // // Sin esto, no actualiza la historia clínica;
-    // // pero sí los datos de usuario. Interesante.
-    // setTimeout(() => {
-    //   this.verHistoriaClinica = true;
-    // }, 5);
-    
     this.verHistoriaClinica = true;
   }
   ocultarHandler() {
--- src/app/especialista/pacientes/pacientes.component.ts
+++ src/app/especialista/pacientes/pacientes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UsuarioService } from 'src/app/services/usuario.service';
 
 @Component({
   selector: 'app-pacientes',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pacientes.component.scss']
 })
 export class PacientesComponent implements OnInit {
+  usuarios:Array<any> = [];
+  verHistoriaClinica: boolean = false;
+  pacienteSeleccionado: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private usuarioService:UsuarioService) {
   }
 
+  ngOnInit(): void {
+    this.usuarioService.getUsuarios().subscribe(
+      dcas => {
+        this.usuarios = [];
+
+        dcas.forEach(
+          dca => {
+            const obj:any = dca.payload.doc.data();
+            obj.id = dca.payload.doc.id;
+            this.usuarios.push(obj);
+          }
+        );
+      } 
+    )
+  }
+
+  verHistoriaClinicaHandler(paciente: any) {
+    this.pacienteSeleccionado = paciente;
+    this.verHistoriaClinica = true;
+  }
+  ocultarHandler() {
+    this.verHistoriaClinica = false;
+  }
 }
