4cd1eee701930fa8839ca9ed1a843ae8ea47f358
unknown <cataniafrancoar@gmail.com>
2022-06-23 08:58:26 -0300

    HC en PDF datos solos

--- src/app/shared/historia-clinica/historia-clinica.component.ts
+++ src/app/shared/historia-clinica/historia-clinica.component.ts
@@ -23,18 +23,65 @@ export class HistoriaClinicaComponent implements OnInit {
     this.hc = this.paciente.historiaClinica ? this.paciente.historiaClinica : null;
   }
 
-  createPDF(){
- 
+  createPDF() {
+    const textos: any[] = [];
+
+    if (this.hc) {
+      const paresHC = Object.entries(this.hc);
+
+      paresHC.forEach(
+        parHC => textos.push({text: '\n' + parHC[0] + ': ' + parHC[1]})
+      )
+    }
+
     const pdfDefinition: any = {
       content: [
         {
-          text: 'Hola mundo',
-        }
-      ]
+          text: 'Historia clínica',
+          style: 'titulo',
+          alignment: 'center'
+        },
+        {
+          text: 'Datos personales',
+          style: 'subtitulo1'
+        },
+        {
+          text: [
+              'Paciente: ' + this.paciente.nombre + ' ' + this.paciente.apellido,
+              '\n\nEdad: ' + this.paciente.edad,
+              '\n\nDNI: ' + this.paciente.dni,
+              '\n\nObra Social: ' + this.paciente.obraSocial,
+              '\n\n'
+            ]
+        },
+        {
+          text: 'Datos médicos',
+          style: 'subtitulo2'
+        },
+        ...textos
+      ],
+      styles: {
+        titulo: {
+			    alignment: 'justify',
+          fontSize: 34,
+          bold: true,
+          margin: [0, 0, 0, 60]
+        },
+        subtitulo1: {
+          fontSize: 20,
+          bold: true,
+          margin: [0, 20]
+        },
+        subtitulo2: {
+          fontSize: 20,
+          bold: true,
+          margin: [0, 20, 10, 0]
+        },
+      }
     }
  
     const pdf = pdfMake.createPdf(pdfDefinition);
-    pdf.open();
- 
+    pdf.open(); 
   }
+
 }
