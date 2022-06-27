95dba6fd2c7ae8a28e07c5dc9487c5c2eaae88c2
unknown <cataniafrancoar@gmail.com>
2022-06-27 11:52:16 -0300

    favbutton-usuarios

--- src/app/especialista/pacientes/pacientes.component.ts
+++ src/app/especialista/pacientes/pacientes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TurnoService } from 'src/app/services/turno.service';
 import { AuthService } from 'src/app/services/auth.service';
+import { DocUsuario } from 'src/app/models/docUsuario';
 
 @Component({
   selector: 'app-pacientes',
@@ -8,7 +9,8 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./pacientes.component.scss']
 })
 export class PacientesComponent implements OnInit {
-  usuarios:Array<any> = [];
+  // usuarios:Array<any> = [];
+  docsUsuario: DocUsuario[] = [];
   verHistoriaClinica: boolean = false;
   pacienteSeleccionado: any;
 
@@ -26,28 +28,58 @@ export class PacientesComponent implements OnInit {
         .get()
         .then(
           qs => {
-            const auxUsuarios: Array<any> = [];
-
             qs.forEach(doc => {
               const objeto: any = doc.data();
               const idPaciente = objeto.idPac;
               const paciente = objeto.paciente;
 
-              const some = auxUsuarios.some(
-                auxUsuario => auxUsuario.idPac == idPaciente
+              const some = this.docsUsuario.some(
+                auxUsuario => auxUsuario.id == idPaciente
               );
 
               if (!some) {
-                auxUsuarios.push({idPac: idPaciente, paciente: paciente});
+                this.docsUsuario.push({id: idPaciente, usuario: paciente});
               }
             });
-
-            this.usuarios = auxUsuarios.map(
-              auxUsuario => auxUsuario.paciente
-            );
       })
     })
   }
+
+  usuarioSeleccionadoHandler(docUsuario: DocUsuario) {
+    console.log(docUsuario);
+  }
+
+  // ngOnInit(): void {
+  //   this.authService.getUserID().subscribe(
+  //     uid => {
+  //       this.turnoService.getRef()
+  //       .where('idEsp', '==', uid)
+  //       .where('estado', '==', 'realizado')
+  //       .get()
+  //       .then(
+  //         qs => {
+  //           const auxUsuarios: Array<any> = [];
+
+  //           qs.forEach(doc => {
+  //             const objeto: any = doc.data();
+  //             const idPaciente = objeto.idPac;
+  //             const paciente = objeto.paciente;
+
+  //             const some = auxUsuarios.some(
+  //               auxUsuario => auxUsuario.idPac == idPaciente
+  //             );
+
+  //             if (!some) {
+  //               auxUsuarios.push({idPac: idPaciente, paciente: paciente});
+  //             }
+  //           });
+
+  //           this.usuarios = auxUsuarios.map(
+  //             auxUsuario => auxUsuario.paciente
+  //           );
+  //     })
+  //   })
+  // }
 
   verHistoriaClinicaHandler(paciente: any) {
     this.pacienteSeleccionado = paciente;
