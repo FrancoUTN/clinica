aec30a421cff8f9eb20a01fa73115718aa999f78
unknown <cataniafrancoar@gmail.com>
2022-06-28 16:28:01 -0300

    Turnos por especialidad con pipe y switchMap

--- src/app/administrador/informes/turnos-especialidad/turnos-especialidad.component.ts
+++ src/app/administrador/informes/turnos-especialidad/turnos-especialidad.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { DocUsuario } from 'src/app/models/DocUsuario';
 import { TurnoService } from 'src/app/services/turno.service';
-import { UsuarioService } from 'src/app/services/usuario.service';
 import * as Highcharts from 'highcharts';
 import { Turno } from 'src/app/models/Turno';
 import { EspecialidadService } from 'src/app/services/especialidad.service';
 import { Especialidad } from 'src/app/models/Especialidad';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-turnos-especialidad',
@@ -51,75 +50,51 @@ export class TurnosEspecialidadComponent implements OnInit {
     private especialidadService: EspecialidadService) { }
 
   ngOnInit(): void {
-    this.especialidadService.getEspecialidades().subscribe(
-      qs => {
-        qs.forEach(
-          qds => {
-            const nombre = qds.data().nombre;
-            const imagen = qds.data().imagen;
+    this.especialidadService.getEspecialidades().pipe(
+      switchMap(
+        qs => {
+          qs.forEach(
+            qds => {
+              const nombre = qds.data().nombre;
+              const imagen = qds.data().imagen;
+  
+              this.especialidades.push({nombre: nombre, imagen: imagen});
+            }
+          );  
 
-            this.especialidades.push({nombre: nombre, imagen: imagen});
-          }
-        );
-
-      }
-    );
-
-    this.turnoService.getRef().get().then(
-      qs => {
-        qs.forEach(
+          return this.turnoService.getRef().get()
+        }
+      ),
+      map(
+        qs => qs.forEach(
           qds => {
             const data: any = qds.data();
             this.turnos.push(data);
           }
-        );
-      }
-    );
-    // this.turnoService.getRef().get().then(
-    //   qs => {
-    //     qs.forEach(
-    //       qds => {
-    //         const data: any = qds.data();
-    //         const esp: string = data.especialidad;
-    //         let cantidad = 0;
+        )
+      )
+    ).subscribe(
+      () => {
+        this.especialidades.forEach(
+          especialidad => {
+            this.strEspecialidades.push(especialidad.nombre);
+            let cantidad = 0;
 
-    //         this.especialidades.forEach(
-    //           (especialidad, indice) => {
-    //             if (esp === especialidad.nombre) {
-    //               cantidad++;
-    //             }
-    //           }
-    //         );
+            this.turnos.forEach(
+              turno => {
+                if (turno.especialidad === especialidad.nombre) {
+                  cantidad++;
+                }
+              }
+            );
 
-    //         this.strEspecialidades.push()
-    //         this.cantidades.push(cantidad);
-
-
-    //       }
-    //     )
-    //   }
-    // )
-  }
-
-  accionar() {
-    this.especialidades.forEach(
-      especialidad => {
-        this.strEspecialidades.push(especialidad.nombre);
-        let cantidad = 0;
-
-        this.turnos.forEach(
-          turno => {
-            if (turno.especialidad === especialidad.nombre) {
-              cantidad++;
-            }
+            this.cantidades.push(cantidad);
           }
         );
 
-        this.cantidades.push(cantidad);
+        this.renderizar = true;
       }
     );
-
-    this.renderizar = true;
   }
 
 }
