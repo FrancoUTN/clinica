8b5f024c45e38886b4eda21c269ed63998edcbe3
unknown <cataniafrancoar@gmail.com>
2022-06-28 10:18:48 -0300

    fechas between

--- src/app/administrador/informes/turnos-finalizados/turnos-finalizados.component.ts
+++ src/app/administrador/informes/turnos-finalizados/turnos-finalizados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TurnoService } from 'src/app/services/turno.service';
 
 @Component({
   selector: 'app-turnos-finalizados',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./turnos-finalizados.component.scss']
 })
 export class TurnosFinalizadosComponent implements OnInit {
+  start = new Date('2022-01-01');
+  end = new Date('2022-12-12');
 
-  constructor() { }
+  constructor(private turnoService: TurnoService) { }
 
   ngOnInit(): void {
+    this.turnoService.getRef()
+      .where('fecha', '>', this.start)
+      .where('fecha', '<', this.end)
+      .onSnapshot(
+        qs => qs.forEach(
+          qds => {
+            console.log(qds.data());
+          }
+        )
+      );
   }
 
 }
