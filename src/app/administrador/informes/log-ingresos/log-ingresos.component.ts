b0a00b4f0a9867d3750fe5eddd95a25b100ea7fe
unknown <cataniafrancoar@gmail.com>
2022-06-29 08:11:30 -0300

    Ingresos en Excel

--- src/app/administrador/informes/log-ingresos/log-ingresos.component.ts
+++ src/app/administrador/informes/log-ingresos/log-ingresos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Ingreso } from 'src/app/models/Ingreso';
 import { IngresoService } from 'src/app/services/ingreso.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-log-ingresos',
@@ -9,7 +10,7 @@ import { IngresoService } from 'src/app/services/ingreso.service';
 })
 export class LogIngresosComponent implements OnInit {
   ingresos: Ingreso[] = [];
-  
+
   constructor(private ingresoService: IngresoService) { }
 
   ngOnInit(): void {
@@ -26,4 +27,16 @@ export class LogIngresosComponent implements OnInit {
     );
   }
 
+  exportarExcel(elementId: string, nombreArchivo: string) {
+    /* pass here the table id */
+    let element = document.getElementById(elementId);
+    const ws: XLSX.WorkSheet =XLSX.utils.table_to_sheet(element);
+ 
+    /* generate workbook and add the worksheet */
+    const wb: XLSX.WorkBook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Sheet1');
+ 
+    /* save to file */
+    XLSX.writeFile(wb, nombreArchivo + '.xlsx'); 
+  }
 }
