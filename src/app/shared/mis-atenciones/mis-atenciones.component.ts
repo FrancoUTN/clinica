f7ea61d58ae19794d81ec9f0c0586ea9991046d3
unknown <cataniafrancoar@gmail.com>
2022-06-29 16:20:07 -0300

    atenciones segun un PROFESIONAL

--- src/app/shared/mis-atenciones/mis-atenciones.component.ts
+++ src/app/shared/mis-atenciones/mis-atenciones.component.ts
@@ -1,10 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { TurnoService } from 'src/app/services/turno.service';
 import { AuthService } from 'src/app/services/auth.service';
 import { DocUsuario } from 'src/app/models/DocUsuario';
 import { Usuario } from 'src/app/models/Usuario';
 import { Turno } from 'src/app/models/Turno';
-import { map, switchMap } from 'rxjs';
+import { map } from 'rxjs';
+import { logoEnBase64 } from 'src/app/others/logo-en-base-64';
+
+const pdfMake = require('pdfmake/build/pdfmake.js');
+const pdfFonts = require("pdfmake/build/vfs_fonts");
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
 @Component({
   selector: 'app-mis-atenciones',
@@ -12,6 +17,7 @@ import { map, switchMap } from 'rxjs';
   styleUrls: ['./mis-atenciones.component.scss']
 })
 export class MisAtencionesComponent implements OnInit {
+  @Output() volver = new EventEmitter();
   miUid!: string;  
   docsEspecialista: DocUsuario[] = [];
   especialistaSeleccionado!: Usuario;
@@ -24,7 +30,6 @@ export class MisAtencionesComponent implements OnInit {
   }
 
   ngOnInit(): void {
-
     this.authService.getUserID().pipe(
       map(
         (userID: string) => {
@@ -55,11 +60,12 @@ export class MisAtencionesComponent implements OnInit {
           })
         );
       }
-    );    
-
+    );
   }
 
   usuarioSeleccionadoHandler(docUsuario: DocUsuario) {
+    this.especialistaSeleccionado = docUsuario.usuario;
+
     this.turnoService.getRef()
       .where('idPac', '==', this.miUid)
       .where('estado', '==', 'realizado')
@@ -76,8 +82,78 @@ export class MisAtencionesComponent implements OnInit {
             }
           );
 
-          this.verTurnos = true;
+          // this.verTurnos = true;
+          this.crearPDF();
         }
       );
+  }
+  
+  crearPDF() {
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: '2-digit',
+      minute: '2-digit'
+    };
+
+    const arrayDeArrays:Array<Array<string>> = [];
+    const doctor = this.especialistaSeleccionado.apellido + ', ' + this.especialistaSeleccionado.nombre;
+    
+    this.turnos.forEach(
+      turno => {
+          const horario = turno.fecha.toDate().toLocaleString('es-ES', options);
+
+          const datosUtiles: string[] = [
+            horario,
+            turno.especialidad,
+            turno.reviewEsp
+          ];
+
+          arrayDeArrays.push(datosUtiles);
+      }
+    );
+
+    const dd: any = {
+      content: [
+        {
+          text: 'Historial de atenciones con el Dr. ' + doctor + ':',
+          style: 'header'
+        },
+        {
+          style: 'tableExample',
+          table: {
+            body: [
+              ['Horario', 'Especialidad', 'Reseña del especialista'],
+              ...arrayDeArrays
+            ]
+          }
+        },
+        {
+          image: logoEnBase64,
+          width: 200,
+          style: 'logo',
+          alignment: 'center',
+          opacity: 0.5
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true,
+          margin: [0, 0, 0, 10]
+        },
+        tableExample: {
+          margin: [0, 5, 0, 15]
+        },
+        logo: {
+			    alignment: 'justify',
+        }
+      }
+    };
+    
+    const pdf = pdfMake.createPdf(dd);
+    pdf.open();
   }
 }
