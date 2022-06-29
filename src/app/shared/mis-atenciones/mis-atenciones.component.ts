26f1a468adc7baeaaf511474d5c5535f580b7802
unknown <cataniafrancoar@gmail.com>
2022-06-29 15:10:50 -0300

    mis-atenciones fav-buttons

--- src/app/shared/mis-atenciones/mis-atenciones.component.ts
+++ src/app/shared/mis-atenciones/mis-atenciones.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { TurnoService } from 'src/app/services/turno.service';
+import { AuthService } from 'src/app/services/auth.service';
+import { DocUsuario } from 'src/app/models/DocUsuario';
+import { Usuario } from 'src/app/models/Usuario';
+import { Turno } from 'src/app/models/Turno';
+import { map, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-mis-atenciones',
@@ -6,10 +12,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./mis-atenciones.component.scss']
 })
 export class MisAtencionesComponent implements OnInit {
+  miUid!: string;  
+  docsEspecialista: DocUsuario[] = [];
+  especialistaSeleccionado!: Usuario;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private turnoService: TurnoService,
+    private authService: AuthService) {
   }
 
+  ngOnInit(): void {
+
+    this.authService.getUserID().pipe(
+      map(
+        (userID: string) => {
+          return this.turnoService.getRef()
+            .where('idPac', '==', userID)
+            .where('estado', '==', 'realizado')
+            .get()
+        }
+      )
+    )
+    .subscribe(
+      promesa => {
+        promesa.then(
+          qs => qs.forEach(doc => {
+            const objeto: any = doc.data();
+            const idEspecialista = objeto.idEsp;
+            const especialista = objeto.especialista;
+  
+            const some = this.docsEspecialista.some(
+              esp => idEspecialista === esp.id
+            );
+  
+            if (!some) {
+              this.docsEspecialista.push({id: idEspecialista, usuario: especialista});
+            }
+          })
+        );
+      }
+    );    
+
+  }
+
+  usuarioSeleccionadoHandler(algo:any) {
+
+  }
 }
